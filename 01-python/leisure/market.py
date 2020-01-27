5265244d7738f599f8048b48b2ac621a59ba2bec
Qi Yin <1052317189@qq.com>
2020-01-27 17:34:17 -0500

    add a market and make te height of the market variable

--- 01-python/leisure/market.py
+++ 01-python/leisure/market.py
@@ -1,3 +1,16 @@
-def draw_market():
-    print("market not found")
+def draw_market(height = 10):
+    if(height == 0): 
+        print("market height is invalid!")
+        return
+    print("market is here!")
+    print("        _________________________")
+    print("       /\                         \ ")
+    print("      /  \      M A R K E T        \ ")
+    print("     /    \                         \ ")
+    print("    /______\_________________________\ ")
+
+    
+    for i in range(height-1):
+        print("    |       |                  |     |")
+    print("    |_______|__________________|_____|")
     return
