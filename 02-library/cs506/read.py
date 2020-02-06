513afb44550812492b0e8fdb7091de85ccf999f2
Qi Yin <1052317189@qq.com>
2020-02-05 19:22:19 -0500

    finish 02 tasks

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,16 @@
+import csv
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
+    data = []
+    datafile = open(csv_file_path)
+    csv_reader = csv.reader(datafile)
+    for row in csv_reader:
+            r = []
+            for i in range(len(row)):
+                    r.append(int(row[i]))
+            data.append(r)
+    return data
     raise NotImplementedError()
