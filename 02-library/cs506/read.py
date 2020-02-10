fd47e5c33182a053a10059ede29f28114c9040d4
Qi Yin <1052317189@qq.com>
2020-02-10 17:45:44 -0500

    exercise in class

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -4,6 +4,7 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
+    """   
     data = []
     datafile = open(csv_file_path)
     csv_reader = csv.reader(datafile)
@@ -12,5 +13,11 @@ def read_csv(csv_file_path):
             for i in range(len(row)):
                     r.append(int(row[i]))
             data.append(r)
-    return data
+    """
+    res = []
+    with open(csv_file_path, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+                res.append([eval(x) for x in line.split(",")])
+    return res
     raise NotImplementedError()
